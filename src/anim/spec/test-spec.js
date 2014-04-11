8f608401e972c40ea02cf49e1ef945d9c3ed662c
Christoph Altmann <christoph.altmann@platogo.com>
2014-04-11 17:26:59 +0200

    Refactor tests in getAngle describe Block

--- src/anim/spec/test-spec.js
+++ src/anim/spec/test-spec.js
@@ -9,27 +9,30 @@ function getAngle(counter){
 }
 
 describe('Compass', function() {
-  describe('counter: 1', function(){
-    it('should return 1°', function() {
-      expect(getAngle(1)).toBe(1);
+  describe('getAngle', function(){
+    describe('counter: 1', function(){
+      it('should return 1°', function() {
+        expect(getAngle(1)).toBe(1);
+      });
+    });
+
+    describe('counter: 2', function(){
+      it('should return 2°', function() {
+        expect(getAngle(2)).toBe(2);
+      });
+    });
+
+    describe('counter: 360', function(){
+      it('should return 0°', function() {
+        expect(getAngle(360)).toBe(0);
+      });
+    });
+
+    describe('counter: -1', function(){
+      it('should return 359°', function() {
+        expect(getAngle(-1)).toBe(359);
+      });
     });
   });
 
-  describe('counter: 2', function(){
-    it('should return 2°', function() {
-      expect(getAngle(2)).toBe(2);
-    });
-  });
-
-  describe('counter: 360', function(){
-    it('should return 0°', function() {
-      expect(getAngle(360)).toBe(0);
-    });
-  });
-
-  describe('counter: -1', function(){
-    it('should return 359°', function() {
-      expect(getAngle(-1)).toBe(359);
-    });
-  });
 });
