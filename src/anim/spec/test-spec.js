f946745e5eca74840d920e52fb8bc28fc5a23faf
Christoph Altmann <christoph.altmann@platogo.com>
2014-04-11 17:21:18 +0200

    Pass test getAngle(2)

--- src/anim/spec/test-spec.js
+++ src/anim/spec/test-spec.js
@@ -1,11 +1,17 @@
-function getAngle(){
-  return 1;
+function getAngle(counter){
+  return counter;
 }
 
 describe('Compass', function() {
   describe('counter: 1', function(){
     it('should return 1°', function() {
       expect(getAngle(1)).toBe(1);
+    });
+  });
+
+  describe('counter: 2', function(){
+    it('should return 2°', function() {
+      expect(getAngle(2)).toBe(2);
     });
   });
 });
