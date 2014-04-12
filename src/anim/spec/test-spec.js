fa3c313e0acafcd5ea930261a402ab57c94449bc
Christoph Altmann <christoph.altmann@platogo.com>
2014-04-12 07:16:46 +0200

    Refactor getAngle into image

--- src/anim/spec/test-spec.js
+++ src/anim/spec/test-spec.js
@@ -1,14 +1,5 @@
 var jQuery = require('jquery');
 
-function getAngle(counter){
-  angle = counter % 360;
-
-  if(angle < 0){
-    angle = 360 + angle;
-  }
-
-  return angle;
-}
 
 
 function Image(textId, imgId){
@@ -19,32 +10,45 @@ function Image(textId, imgId){
 Image.prototype = {
   showText: function(domId, angle){
     jQuery('#' + this.textId).html(angle)
+  },
+
+  getAngle: function(counter){
+  angle = counter % 360;
+
+  if(angle < 0){
+    angle = 360 + angle;
   }
+
+  return angle;
+}
 }
 
 describe('Compass', function() {
   describe('getAngle', function(){
+    beforeEach(function(){
+      image = new Image()
+    })
     describe('counter: 1', function(){
       it('should return 1°', function() {
-        expect(getAngle(1)).toBe(1);
+        expect(image.getAngle(1)).toBe(1);
       });
     });
 
     describe('counter: 2', function(){
       it('should return 2°', function() {
-        expect(getAngle(2)).toBe(2);
+        expect(image.getAngle(2)).toBe(2);
       });
     });
 
     describe('counter: 360', function(){
       it('should return 0°', function() {
-        expect(getAngle(360)).toBe(0);
+        expect(image.getAngle(360)).toBe(0);
       });
     });
 
     describe('counter: -1', function(){
       it('should return 359°', function() {
-        expect(getAngle(-1)).toBe(359);
+        expect(image.getAngle(-1)).toBe(359);
       });
     });
   });
